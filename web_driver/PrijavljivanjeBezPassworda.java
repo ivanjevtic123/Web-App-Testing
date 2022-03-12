a46bf2a8e8390fffba861dca70f7a9611534f86f
ivanjevtic <ji180550d@student.etf.bg.ac.rs>
2022-03-12 11:50:37 +0100

    WebApp testing final commit

---
+++ web_driver/PrijavljivanjeBezPassworda.java
@@ -0,0 +1,43 @@
+package ts_dz1_web;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PrijavljivanjeBezPassworda {
+	public static void main(String[] args) {
+		try {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\korisnik\\Desktop\\TS_dz1_web\\jarFolder\\chromedriver.exe");
+			
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--incognito");
+			
+			WebDriver web = new ChromeDriver(options);
+			web.get("http://localhost/projekat4/ankete2/login.php");
+			web.manage().window().maximize();
+			
+			WebElement user = web.findElement(By.name("username1"));
+			user.sendKeys("Luka1996");
+			
+			Thread.sleep(2100);
+			
+			WebElement pass = web.findElement(By.name("password1"));
+			pass.sendKeys("");
+			
+			WebElement btn1 = web.findElement(By.name("logindugme"));
+			btn1.click();
+			//System.out.println("proba");
+			//validation point
+			WebElement provera = web.findElement(By.linkText("LOGUOT"));//Login
+			System.out.println("TestNeuspesan - korisnik se ulogovao");
+			
+			Thread.sleep(2100);
+			web.close();
+		} catch (Exception e) {
+			//System.err.println(e.getMessage());
+			System.out.println("Test uspesan - korisnik nije uspeo da se uloguje");
+		}
+	}
+}
